b9d9a1d9826cf69e2c4b23e7aa9ce7158526e68e
Keyurkarenai2 <karenakeyur20@gmail.com>
2025-11-06 18:13:00 +0530

    package update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider, useDispatch } from 'react-redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { store } from './store/store';
 import Navbar from './components/layout/Navbar';
