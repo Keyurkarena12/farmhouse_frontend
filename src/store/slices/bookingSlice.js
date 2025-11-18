7df0370f4f3c92df9931e8dab169e8fa05ba7435
Keyurkarenai2 <karenakeyur20@gmail.com>
2025-11-18 14:55:03 +0530

    live url add

--- src/store/slices/bookingSlice.js
+++ src/store/slices/bookingSlice.js
@@ -432,7 +432,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'https://farmhouse-backend.vercel.app/api';
+const API_URL = 'https://farmhouse-backend-clean.vercel.app/api';
 // const API_URL = 'http://localhost:5000/api';
 
 
